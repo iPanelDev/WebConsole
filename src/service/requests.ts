fc553997e40ee111dcfd0831799ebea0c9ada12a
Zaitonn <103164490+Zaitonn@users.noreply.github.com>
2023-09-30 19:13:06 +0800

    fix: 依赖错误

--- src/service/requests.ts
+++ src/service/requests.ts
@@ -2,45 +2,12 @@ import { useServiceStore } from "@/service/store";
 import { Instance, SimplePacket, Status, User } from "@/service/types";
 import axios from "axios";
 import md5 from "blueimp-md5";
-
-async function getData(url: string, init?: RequestInit) {
-    return (await (await fetch(url, init)).json()) as SimplePacket;
-}
-
-async function deleteData(url: string, init?: RequestInit) {
-    init ??= {};
-    return (await (
-        await fetch(url, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                ...init.headers,
-            },
-            ...init,
-        })
-    ).json()) as SimplePacket;
-}
-
-async function postJson(url: string, body: Object, init?: RequestInit) {
-    init ??= {};
-    return (await (
-        await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                ...init.headers,
-            },
-            body: JSON.stringify(body),
-            ...init,
-        })
-    ).json()) as SimplePacket;
-}
-
 /**
  * 生成uuid
  */
 async function generateUUID() {
-    return (await getData("/api/generateUUID")).data as string;
+    return (await axios.get<SimplePacket>("/api/generateUUID")).data
+        .data as string;
 }
 
 /**
@@ -49,16 +16,30 @@ async function generateUUID() {
 export async function login() {
     const serviceStore = useServiceStore();
 
-    const { code, data } = await postJson("/api/login", {
-        user_name: serviceStore.userName,
-        token: md5(
-            (await generateUUID()) +
-                serviceStore.userName +
-                serviceStore.password
-        ),
-    });
+    const {
+        status,
+        data: { data },
+    } = await axios.postForm<SimplePacket>(
+        "/api/login",
+        JSON.stringify({
+            user_name: serviceStore.userName,
+            token: md5(
+                (await generateUUID()) +
+                    serviceStore.userName +
+                    serviceStore.password
+            ),
+        }),
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            validateStatus: () => true,
+        }
+    );
 
-    if (code !== 200) throw new Error(`登录失败：${data}`);
+    if (status !== 200) {
+        throw new Error(`登录失败：${data}`);
+    }
 
     return data as Status;
 }
@@ -67,28 +48,34 @@ export async function login() {
  * 获取登录态
  */
 export async function getStatus() {
-    return (await getData("/api/status")).data as Status;
+    return (await axios.get<SimplePacket>("/api/status")).data.data as Status;
 }
 
 /**
  * 获取当前用户信息
  */
 export async function getUserInfo() {
-    return (await getData("/api/user/current")).data as User;
+    return (await axios.get<SimplePacket>("/api/user/current")).data
+        .data as User;
 }
 
 /**
  * 获取当前用户信息
  */
 export async function listUsers() {
-    return (await getData("/api/user/all")).data as Record<string, User>;
+    return (await axios.get<SimplePacket>("/api/user/all")).data.data as Record<
+        string,
+        User
+    >;
 }
 
 /**
  * 删除用户
  */
 export async function deleteUser(userName: string) {
-    return await deleteData(`/api/user/${encodeURIComponent(userName)}`);
+    return await axios.delete<SimplePacket>(
+        `/api/user/${encodeURIComponent(userName)}`
+    );
 }
 
 /**
